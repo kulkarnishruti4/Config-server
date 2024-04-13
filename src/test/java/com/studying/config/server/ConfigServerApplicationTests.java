a6b8ea75562d7e5158065dd45e2d358295bbee0f
Shruti Kulkarni <166386268+kulkarnishruti4@users.noreply.github.com>
2024-04-13 23:20:07 +0530

    Initial commit

---
+++ src/test/java/com/studying/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studying.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
